3f223c9a2943493eaf2cce5884f55dd9e1ff33e2
M <m@MacBook-Air-mjed.local>
2025-12-29 00:04:00 +0300

    merge dev with jumanah

--- src/main/java/org/example/sijalsystem/Repository/CustomerRepository.java
+++ src/main/java/org/example/sijalsystem/Repository/CustomerRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface CustomerRepository extends JpaRepository<Customer , Integer> {
 
     Customer findCustomerById(Integer id);
+
+    Customer findCustomerByUser_Id(Integer userId);
 }
